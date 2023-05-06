06dda5a938f1491bf9bd87352fed2ff8c59ce6ea
HuangYiLun <wk5018102@gmail.com>
2023-05-06 18:43:40 +0800

    feat: add new.hbs & records routes

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
+const Category = require('../../models/category')
+
 //定義首頁路由
-router.get('/', (req, res) => {
-  res.render('new')
+router.get('/', async (req, res) => {
+  try {
+
+    const categories = await Category.find().lean().sort({ _id: 'asc' })
+
+    res.render('new', { categories })
+
+  } catch (error) {
+    console.warn(`routes/modules/records/get:${error}`)
+  }
+
 })
 
 module.exports = router
